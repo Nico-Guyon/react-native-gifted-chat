0672be45578c4716e6718f0fcced73e87cda03c6
Nicolas Guyon <nicolas.guyon@telecomnancy.net>
2020-10-10 15:56:42 +0200

    Small change for modifying default colors
    By exporting a class instance, if the main application import Color
    before react-native-gifted-chat it is possible to change
    the default colors used.

--- src/Color.ts
+++ src/Color.ts
@@ -1,17 +1,39 @@
-export default {
-  defaultColor: '#b2b2b2',
-  backgroundTransparent: 'transparent',
-  defaultBlue: '#0084ff',
-  leftBubbleBackground: '#f0f0f0',
-  black: '#000',
-  white: '#fff',
-  carrot: '#e67e22',
-  emerald: '#2ecc71',
-  peterRiver: '#3498db',
-  wisteria: '#8e44ad',
-  alizarin: '#e74c3c',
-  turquoise: '#1abc9c',
-  midnightBlue: '#2c3e50',
-  optionTintColor: '#007AFF',
-  timeTextColor: '#aaa',
-}
+class ChatColors {
+  defaultColor: string;
+  backgroundTransparent: string;
+  defaultBlue: string; 
+  leftBubbleBackground: string; 
+  black: string; 
+  white: string; 
+  carrot: string; 
+  emerald: string; 
+  peterRiver: string; 
+  wisteria: string; 
+  alizarin: string; 
+  turquoise: string; 
+  midnightBlue: string; 
+  optionTintColor: string; 
+  timeTextColor: string; 
+  
+  constructor(){
+    this.defaultColor = '#b2b2b2';
+    this.backgroundTransparent = 'transparent';
+    this.defaultBlue = '#0084ff';
+    this.leftBubbleBackground = '#f0f0f0';
+    this.black = '#000';
+    this.white = '#fff';
+    this.carrot = '#e67e22';
+    this.emerald = '#2ecc71';
+    this.peterRiver = '#3498db';
+    this.wisteria = '#8e44ad';
+    this.alizarin = '#e74c3c';
+    this.turquoise = '#1abc9c';
+    this.midnightBlue = '#2c3e50';
+    this.optionTintColor = '#007AFF';
+    this.timeTextColor = '#aaa';
+  }
+} 
+
+const colors = new ChatColors();
+
+export default colors; 
